5101565e2b4d4efe65a1198ecfb2cbee96ccaba6
Anderson Andrei Da Silva <dasilvaa@dasilvaa.lan>
2024-09-25 11:05:56 -0700

    Update copyrights

--- wfbench/knative-translator/knative.py
+++ wfbench/knative-translator/knative.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2021-2022 The WfCommons Team.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright 2024 Hewlett Packard Enterprise Development LP.
 
 import pathlib
 import json
